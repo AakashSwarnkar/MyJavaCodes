6326d7c1a1771febb399c5aeb8e1ddd08091793d
AakashSwarnkar <aakash.swarnkar26@gmail.com>
2024-06-29 16:48:14 +0530

    Add new java file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # MyJavaCodes
 Find my all practice and lectures java codes. 
 <br>
-Author - Aakash Swarnkar
+Author - Aakash
---
+++ print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class print {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your name : ");
+        String name = input.next();
+    }
+}
